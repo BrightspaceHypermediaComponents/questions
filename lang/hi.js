fd5848b1c63fbc973da780f331b8b3c57dcce74e
JenkinsReader <resupport@desire2learn.com>
2022-02-24 19:29:55 -0500

    Translation Updates

---
+++ lang/hi.js
@@ -0,0 +1,8 @@
+/* eslint quotes: 0 */
+
+export default {
+	"correctAnswer": "Correct Answer",
+	"incorrectAnswer": "Incorrect Answer",
+	"correctResponse": "Correct Response",
+	"incorrectResponse": "Incorrect Response"
+};
