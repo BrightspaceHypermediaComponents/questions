092d783d775f0709dda8446aea3e2c2728844845
Girujan Naguleswaran <girujan.naguleswaran@d2l.com>
2022-02-28 11:13:43 -0500

    Rendering fields with data from JSON files

--- components/d2l-questions-written-response.js
+++ components/d2l-questions-written-response.js
@@ -1,7 +1,8 @@
 import '@brightspace-ui/core/components/inputs/input-textarea.js';
+import 'd2l-polymer-siren-behaviors/store/entity-store.js';
 import { bodyStandardStyles, labelStyles } from '@brightspace-ui/core/components/typography/styles.js';
+import { Classes, Rels } from 'd2l-hypermedia-constants';
 import { css, html, LitElement } from 'lit-element/lit-element.js';
-import { Classes } from 'd2l-hypermedia-constants';
 import { LocalizeDynamicMixin } from '@brightspace-ui/core/mixins/localize-dynamic-mixin.js';
 import { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
 
@@ -13,6 +14,7 @@ class D2lQuestionWrittenResponse extends LocalizeDynamicMixin(LitElement) {
 			question: { type: Object },
 			questionResponse: { type: Object },
 			token: { type: Object },
+			_answerKey: { type: String }
 		};
 	}
 
@@ -24,8 +26,12 @@ class D2lQuestionWrittenResponse extends LocalizeDynamicMixin(LitElement) {
 			:host([hidden]) {
 				display: none;
 			}
-			.d2l-questions-written-response-question-text > p {
+			/* Temp Commented Out Until <p> tag in <d2l-html-block> change implemented */
+			/* .d2l-questions-written-response-question-text > p {
 				margin-bottom: 0.95rem;
+			} */
+			.d2l-questions-written-response-question-initial-text {
+				margin: 0.95rem 0.475rem 0.475rem;
 			}
 			.d2l-questions-written-response-question-answer-key {
 				margin-bottom: 0.8rem;
@@ -57,26 +63,71 @@ class D2lQuestionWrittenResponse extends LocalizeDynamicMixin(LitElement) {
 					${this._renderInitialText()}
 				</div>
 				<div class="d2l-questions-written-response-question-answer-key">
-					<h2 class="d2l-label-text">
-						Answer Key
-					</h2>
-					<d2l-html-body>
-						Answer Key Is The Following (Hardcoded)
-					</d2l-html-body>
+					${this._renderAnswerKey()}
 				</div>
 			</div>
 		`;
 	}
 
-	_renderInitialText() {
-		const temp = this.questionResponse.entity.getSubEntityByClass(Classes.questions.candidateResponse).entities;
-		console.log(temp);
+	async updated(changedProperties) {
+		super.updated();
+		if ((changedProperties.has('question') || changedProperties.has('questionResponse'))) {
+			try {
+				await this._loadAnswerKey();
+			} catch (err) {
+				console.error(err);
+				throw new Error('d2l-questions-written-response: Unable to load answer key from question');
+			}
+		}
+	}
+
+	async _getEntityFromHref(targetHref) {
+		return await window.D2L.Siren.EntityStore.fetch(targetHref, this.token);
+	}
+
+	async _loadAnswerKey() {
+		const itemBodyHref = this.question.entity.getSubEntityByRel(Rels.Questions.itemBody);
+		const itemBodyEntity = await this._getEntityFromHref(itemBodyHref);
+		const answerKeyEntity = itemBodyEntity.entity.getSubEntityByClass(Classes.text.richtext);
+
+		if (answerKeyEntity !== undefined) {
+			this._answerKey = answerKeyEntity.properties.html;
+		}
+	}
+
+	_renderAnswerKey() {
+		if (this._answerKey === undefined) {
+			return html``;
+		}
 
 		return html`
-			<d2l-input-textarea label="Initial Text" label-hidden>
-				Initial
-			</d2l-input-textarea>
+			<h2 class="d2l-label-text">
+				Answer Key
+			</h2>
+			<d2l-html-block>
+				${unsafeHTML(this._answerKey)}
+			</d2l-html-block>
 		`;
+	}
+
+	_renderInitialText() {
+		const responseEntities = this.questionResponse.entity.getSubEntityByClass(Classes.questions.candidateResponse);
+		const initialTextResponse = responseEntities.getSubEntityByClass(Classes.text.richtext);
+
+		if (initialTextResponse !== undefined) {
+			return html`
+				<d2l-html-block>
+					${unsafeHTML(initialTextResponse.properties.html)}
+				</d2l-html-block>
+			`;
+		}
+
+		// Not fully sure the condition to check for if student
+		// return html`
+		// 	<d2l-input-textarea label="Initial Text" label-hidden>
+		// 		${initialTextResponseEntity}
+		// 	</d2l-input-textarea>
+		// `;
 	}
 }
 customElements.define('d2l-questions-written-response', D2lQuestionWrittenResponse);
