81d73b793bea7bcdc6c401c6e72b8d60dd569799
JenkinsReader <resupport@desire2learn.com>
2022-04-25 11:41:08 -0400

    Translation Updates

--- lang/es-es.js
+++ lang/es-es.js
@@ -4,7 +4,7 @@ export default {
 	"GB": "GB",
 	"MB": "MB",
 	"answerKey": "Clave de respuestas",
-	"clickToDownloadAttachmentFile": "Haga clic para descargar el archivo adjunto {filename}",
+	"clickToDownloadAttachmentFile": "Haga clic para descargar el archivo adjunto {fileName}",
 	"correctAnswer": "Respuesta correcta",
 	"correctResponse": "Respuesta correcta",
 	"incorrectAnswer": "Respuesta incorrecta",
