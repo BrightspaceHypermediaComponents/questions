730b884977d3d91905cd33638a134cad65a879cf
Tristan Devos <Tristan.Devos@D2L.com>
2022-05-25 14:25:08 -0400

    written response v-diff tests redone, axe tests, component tests

---
+++ test/d2l-questions-written-response-presentational-data.js
@@ -0,0 +1,42 @@
+export const questionText = '<p>Random Question</p>';
+export const responseText = '<p>Chocolate muffin sugar plum muffin cupcake</p>';
+export const answerKey = '<p>Answer Key Is The Following</p>';
+export const wordCount = 8;
+export const emptyResponse = '';
+export const emptyResponseWordCount = 0;
+export const longResponseText = '<p>Biscuit jelly gummi bears pudding croissant dragée jelly-o. Chocolate liquorice jelly pastry jelly beans carrot cake. Cake croissant tiramisu cake sweet roll brownie cupcake fruitcake sweet. Pudding marshmallow halvah chupa chups soufflé chocolate cupcake. Toffee donut liquorice dessert liquorice. Icing shortbread cheesecake sesame snaps cheesecake halvah icing. Croissant muffin chocolate bar fruitcake chocolate. Dragée lemon drops chocolate bar carrot cake danish carrot cake sweet roll powder chocolate bar. Danish chupa chups gummi bears gummies marshmallow bonbon. Pastry candy pie lollipop topping chocolate cake soufflé. Cake brownie pudding tart donut. Brownie jelly chupa chups sesame snaps marzipan bear claw pastry gummi bears pudding. Wafer croissant candy canes bonbon chupa chups.</p>';
+export const longResponseWordCount = 109;
+export const longResponseText2 = '<p>Jelly-o cookie candy pudding brownie bear claw powder tiramisu. Jelly chocolate chocolate pie tootsie roll. Sugar plum soufflé sugar plum marshmallow danish. Ice cream chupa chups carrot cake fruitcake cupcake sweet roll candy canes. Chocolate cake jelly-o soufflé lollipop candy canes muffin sesame snaps lollipop soufflé. Fruitcake dragée tiramisu carrot cake pie wafer gingerbread candy canes. Biscuit fruitcake powder donut cupcake tiramisu jujubes chocolate. Tootsie roll cake chocolate bar macaroon fruitcake marshmallow. Liquorice carrot cake pudding chocolate cake jelly beans cheesecake gummi bears topping. Chocolate lemon drops cupcake pie bonbon chupa chups lemon drops. Cookie apple pie carrot cake pie fruitcake danish carrot cake. Candy brownie cake bear claw biscuit donut shortbread sesame snaps.</p>';
+export const longResponseWordCount2 = 114;
+export const singleAttachment = [{
+	extension: 'png',
+	href: 'http://d2l-semmv08dxue.desire2learn.d2l:44446/d2l/common/viewFile.d2lfile/Database/MTA5NA/ImageAttachment.png?ou=6609',
+	name: 'ImageAttachment.png',
+	size: 14327
+}];
+export const multipleAttachments = [
+	{
+		extension: 'png',
+		href: 'http://d2l-semmv08dxue.desire2learn.d2l:44446/d2l/common/viewFile.d2lfile/Database/MTA5NA/ImageAttachment.png?ou=6609',
+		name: 'ImageAttachment.png',
+		size: 14327
+	},
+	{
+		extension: 'pdf',
+		href: 'http://d2l-semmv08dxue.desire2learn.d2l:44446/d2l/common/viewFile.d2lfile/Database/MTA5MQ/PDFDocumentAttachment.pdf?ou=6609',
+		name: 'PDFDocumentAttachment.pdf',
+		size: 25697
+	},
+	{
+		extension: 'txt',
+		href: 'http://d2l-semmv08dxue.desire2learn.d2l:44446/d2l/common/viewFile.d2lfile/Database/MTA5Mg/TextFileAttachment.txt?ou=6609',
+		name: 'TextFileAttachment.txt',
+		size: 11
+	},
+	{
+		extension: 'docx',
+		href: 'http://d2l-semmv08dxue.desire2learn.d2l:44446/d2l/common/viewFile.d2lfile/Database/MTA5Mw/WordDocumentAttachment.docx?ou=6609',
+		name: 'WordDocumentAttachment.docx',
+		size: 11863
+	}
+];
