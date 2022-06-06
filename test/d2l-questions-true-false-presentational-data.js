0e9ff1261403caff8c1fb73fead70119183e45db
Jamie Tran <Jamie.Tran@D2L.com>
2022-06-06 16:27:49 -0400

    US130375 - Add vdiff tests for true-false question type

---
+++ test/d2l-questions-true-false-presentational-data.js
@@ -0,0 +1,36 @@
+export const choicesUnanswered = [
+	{
+		htmlText: 'True',
+		text: 'True'
+	},
+	{
+		htmlText: 'False',
+		text: 'False'
+	}
+];
+
+export const choicesCorrect = [
+	{
+		htmlText: 'True',
+		text: 'True',
+		selected: true,
+		correct: true
+	},
+	{
+		htmlText: 'False',
+		text: 'False'
+	}
+];
+
+export const choicesIncorrect = [
+	{
+		htmlText: 'True',
+		text: 'True'
+	},
+	{
+		htmlText: 'False',
+		text: 'False',
+		selected: true,
+		correct: false
+	}
+];
